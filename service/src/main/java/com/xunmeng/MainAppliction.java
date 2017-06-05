5b45a1c332e74048b4d5a39e3c6c3718ac0d77a0
luoyoubao <luoyoubao@quancheng-ec.com>
2017-06-05 20:33:51 +0800

    init

---
+++ service/src/main/java/com/xunmeng/MainAppliction.java
@@ -0,0 +1,12 @@
+package com.xunmeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAppliction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppliction.class, args);
+    }
+}
