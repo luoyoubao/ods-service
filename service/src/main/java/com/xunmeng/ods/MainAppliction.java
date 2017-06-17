cad05dcd623351551e15b9f8d2b444a023e4de96
luoyoubao <luoyoubao@quancheng-ec.com>
2017-06-18 01:38:18 +0800

    bugfix

--- service/src/main/java/com/xunmeng/ods/MainAppliction.java
+++ service/src/main/java/com/xunmeng/ods/MainAppliction.java
@@ -2,10 +2,10 @@ package com.xunmeng.ods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//@ComponentScan({ "com.xunmeng.application", "com.xunmeng.ods.*" })
 @SpringBootApplication
-// @EnableZuulProxy
+@EnableZuulProxy
 public class MainAppliction {
 
     public static void main(String[] args) {
